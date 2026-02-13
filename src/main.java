81d137feeb65e0a3257081223c1d51b341c74dff
sekar purwita <sekarpurwita35015@gmail.com>
2026-02-13 22:55:05 +0700

    create Materi class from Database project

---
+++ src/main.java
@@ -0,0 +1,17 @@
+package src;
+
+public class main {
+    public static void main(String[] args) {
+        Materi m1 = new Materi();
+
+        m1.idMateri = 1;
+        m1.judulMateri = "Pengenalan Objek Java";
+        m1.categorykls = "Pemrograman Berbasis Objek";
+        m1.isiMateri = "mengubah ERD menjadi Class dan Object";
+
+        System.out.println("--Hasil Tugas 2: Pembuatan Objek--");
+        m1.tampilkanInfo();
+    
+    }
+    
+}
